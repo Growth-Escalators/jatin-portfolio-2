1bde81b83c85efbc8c6f55f3aa6a375535207fdf
Jatin Agrawal <hello@growthescalators.com>
2026-06-19 16:54:39 +0530

    Revert "Revert to streamlined hero/layout (remove bento, stack, process, testimonials)"
    This reverts commit dccf772d18069b6cace8623e49fc839579ff1b92.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,30 @@
+import ScrollProgress from "./components/ScrollProgress.jsx";
 import Nav from "./components/Nav.jsx";
 import Hero from "./components/Hero.jsx";
-import About from "./components/About.jsx";
+import Snapshot from "./components/Snapshot.jsx";
+import Stack from "./components/Stack.jsx";
 import Expertise from "./components/Expertise.jsx";
+import Process from "./components/Process.jsx";
 import Projects from "./components/Projects.jsx";
 import Results from "./components/Results.jsx";
+import Testimonials from "./components/Testimonials.jsx";
 import Industries from "./components/Industries.jsx";
 import Contact from "./components/Contact.jsx";
 
 export default function App() {
   return (
     <>
+      <ScrollProgress />
       <Nav />
       <main>
         <Hero />
-        <About />
+        <Snapshot />
+        <Stack />
         <Expertise />
+        <Process />
         <Projects />
         <Results />
+        <Testimonials />
         <Industries />
         <Contact />
       </main>
